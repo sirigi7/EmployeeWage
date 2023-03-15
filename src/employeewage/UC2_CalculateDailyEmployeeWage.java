66e364a2cabb6daade63c43373e7574519e4b4d2
sandeep <sirigi.reddy7@gmail.com>
2023-03-15 19:56:27 +0530

    Added: EmployeeWage

---
+++ src/employeewage/UC2_CalculateDailyEmployeeWage.java
@@ -0,0 +1,25 @@
+package employeewage;
+
+public class UC2_CalculateDailyEmployeeWage {
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOUR = 8;
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage System");
+        int employeeCheck = (int) (Math.random() * 10) % 2; //Using RANDOM function to get any random number for Employee presence.
+        if (employeeCheck == 0) {
+            System.out.println("Employee is absent.");
+        } else {
+            int dailyWage = 0;
+            if (employeeCheck == 1) {
+                System.out.println("Employee is present.");
+                dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR; //Calculating daily wage
+            } else {
+                System.out.println("Employee is absent.");
+            }
+            System.out.println("Daily wage of the employee is:  " + dailyWage);
+        }
+    }
+}
+
